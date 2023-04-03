69a8e6210a22c3a0cecbb883d2c916b83df3f83e
ravinduf <ravinduf@rocketmail.com>
2023-04-03 12:34:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react"
+import Modal from "./components/Modal"
+
+function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const close = () => setModalOpen(false)
+  const open = () => setModalOpen(true)
+
+  return (
+    <div>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="save-button"
+        onClick={() => (modalOpen ? close() : open() )}
+      >
+        Launch modal
+      </motion.button>
+
+      <AnimatePresence
+        initial={false}
+        mode="wait"
+      >
+
+        {modalOpen && <Modal modelOpen={modalOpen} handleClose={close} />}
+      </AnimatePresence>
+
+    </div>
+  )
+}
+
+export default App
